ce45035e9080b47c85a98f6305ba283354d35e97
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-07 13:12:23 +0800

    add readJunkCodeTemplates, junkCodeCtx and garbageInst().

---
+++ garbage.go
@@ -0,0 +1,137 @@
+package shield
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"io/fs"
+)
+
+// The role of the junk code is to make the instruction sequence
+// as featureless as possible.
+var (
+	//go:embed junk/*_x86.asm
+	defaultJunkCodeFSx86 embed.FS
+	
+	//go:embed junk/*_x64.asm
+	defaultJunkCodeFSx64 embed.FS
+	
+	defaultJunkCodeX86 = readJunkCodeTemplates(defaultJunkCodeFSx86)
+	defaultJunkCodeX64 = readJunkCodeTemplates(defaultJunkCodeFSx64)
+)
+
+func readJunkCodeTemplates(efs embed.FS) []string {
+	var templates []string
+	err := fs.WalkDir(efs, ".", func(name string, entry fs.DirEntry, _ error) error {
+		if entry.IsDir() {
+			return nil
+		}
+		file, err := efs.Open(name)
+		if err != nil {
+			panic(err)
+		}
+		data, err := io.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		templates = append(templates, string(data))
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return templates
+}
+
+type junkCodeCtx struct {
+	// for replace registers
+	Reg map[string]string
+	
+	// for insert random instruction pair
+	Switch map[string]bool
+	
+	// for random immediate data
+	BYTE  map[string]int8
+	WORD  map[string]int16
+	DWORD map[string]int32
+	QWORD map[string]int64
+	
+	// for random immediate data with [0, 32) and [0, 64)
+	Less32 map[string]int
+	Less64 map[string]int
+}
+
+// the output garbage instruction length is no limit.
+func (gen *Generator) garbageInst() []byte {
+	if gen.opts.NoGarbage {
+		return nil
+	}
+	// random not insert garbage
+	if gen.rand.Intn(10) == 0 {
+		return nil
+	}
+	var numJunkCodes int
+	switch gen.arch {
+	case 32:
+		numJunkCodes = len(gen.getJunkCodeX86())
+	case 64:
+		numJunkCodes = len(gen.getJunkCodeX64())
+	}
+	// dynamically adjust probability
+	switch gen.rand.Intn(2 + numJunkCodes) {
+	case 0:
+		return nil
+	case 1:
+		return gen.garbageMultiByteNOP()
+	default:
+		return gen.garbageTemplate()
+	}
+}
+
+func (gen *Generator) garbageMultiByteNOP() []byte {
+	var nop []byte
+	switch gen.rand.Intn(2) {
+	case 0:
+		nop = []byte{0x90}
+	case 1:
+		nop = []byte{0x66, 0x90}
+	}
+	return nop
+}
+
+func (gen *Generator) garbageTemplate() []byte {
+	var junkCodes []string
+	switch gen.arch {
+	case 32:
+		junkCodes = gen.getJunkCodeX86()
+	case 64:
+		junkCodes = gen.getJunkCodeX64()
+	}
+	// select random junk code template
+	idx := gen.rand.Intn(len(junkCodes))
+	src := junkCodes[idx]
+	asm, err := gen.buildJunkCode(src)
+	if err != nil {
+		panic(err)
+	}
+	// assemble junk code
+	inst, err := gen.assemble(asm)
+	if err != nil {
+		panic(fmt.Sprintf("failed to assemble junk code: %s", err))
+	}
+	return inst
+}
+
+func (gen *Generator) getJunkCodeX86() []string {
+	if len(gen.opts.JunkCodeX86) > 0 {
+		return gen.opts.JunkCodeX86
+	}
+	return defaultJunkCodeX86
+}
+
+func (gen *Generator) getJunkCodeX64() []string {
+	if len(gen.opts.JunkCodeX64) > 0 {
+		return gen.opts.JunkCodeX64
+	}
+	return defaultJunkCodeX64
+}

ad8572d9b80078fe89b847cba61d383450be5301
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-08 14:44:18 +0800

    add methods about buildShield, rename defaultTemplate to defaultShield.

--- shield.go
+++ shield.go
@@ -5,6 +5,8 @@ import (
 	"embed"
 	"encoding/hex"
 	"fmt"
+	"maps"
+	"slices"
 	"strconv"
 	"strings"
 	"text/template"
@@ -15,10 +17,10 @@ var _ embed.FS
 
 var (
 	//go:embed shield/shield_x86.asm
-	defaultTemplateX86 string
+	defaultShieldX86 string
 
 	//go:embed shield/shield_x64.asm
-	defaultTemplateX64 string
+	defaultShieldX64 string
 )
 
 var (
@@ -63,9 +65,9 @@ func (gen *Generator) buildShield(src string) (string, error) {
 	var shield string
 	switch gen.arch {
 	case 32:
-		shield = gen.getTemplateX86()
+		shield = gen.getShieldX86()
 	case 64:
-		shield = gen.getTemplateX64()
+		shield = gen.getShieldX64()
 	}
 	if src != "" {
 		shield = src
@@ -88,6 +90,119 @@ func (gen *Generator) buildShield(src string) (string, error) {
 		return "", fmt.Errorf("failed to build shield source: %s", err)
 	}
 	return buf.String(), nil
+}
+
+func (gen *Generator) getShieldX86() string {
+	tpl := gen.opts.ShieldX86
+	if tpl != "" {
+		return tpl
+	}
+	return defaultShieldX86
+}
+
+func (gen *Generator) getShieldX64() string {
+	tpl := gen.opts.ShieldX64
+	if tpl != "" {
+		return tpl
+	}
+	return defaultShieldX64
+}
+
+func (gen *Generator) buildRandomRegisterMap() map[string]string {
+	var reg []string
+	switch gen.arch {
+	case 32:
+		reg = slices.Clone(registerX86)
+	case 64:
+		reg = slices.Clone(registerX64)
+	}
+	gen.regBox = reg
+	register := make(map[string]string, 16)
+	switch gen.arch {
+	case 32:
+		for _, reg := range registerX86 {
+			register[reg] = gen.selectRegister()
+		}
+	case 64:
+		for _, reg := range registerX64 {
+			register[reg] = gen.selectRegister()
+		}
+		gen.buildLowBitRegisterMap(register)
+	}
+	return register
+}
+
+func (gen *Generator) buildVolatileRegisterMap() map[string]string {
+	var reg []string
+	switch gen.arch {
+	case 32:
+		reg = slices.Clone(regVolatileX86)
+	case 64:
+		reg = slices.Clone(regVolatileX64)
+	}
+	gen.regBox = reg
+	register := make(map[string]string, len(reg))
+	switch gen.arch {
+	case 32:
+		for _, reg := range regVolatileX86 {
+			register[reg] = gen.selectRegister()
+		}
+	case 64:
+		for _, reg := range regVolatileX64 {
+			register[reg] = gen.selectRegister()
+		}
+		gen.buildLowBitRegisterMap(register)
+	}
+	return register
+}
+
+func (gen *Generator) buildNonvolatileRegisterMap() map[string]string {
+	var reg []string
+	switch gen.arch {
+	case 32:
+		reg = slices.Clone(regNonvolatileX86)
+	case 64:
+		reg = slices.Clone(regNonvolatileX64)
+	}
+	gen.regBox = reg
+	register := make(map[string]string, len(reg))
+	switch gen.arch {
+	case 32:
+		for _, reg := range regNonvolatileX86 {
+			register[reg] = gen.selectRegister()
+		}
+	case 64:
+		for _, reg := range regNonvolatileX64 {
+			register[reg] = gen.selectRegister()
+		}
+		gen.buildLowBitRegisterMap(register)
+	}
+	return register
+}
+
+func (gen *Generator) buildLowBitRegisterMap(register map[string]string) {
+	// build register map about low dword
+	low := make(map[string]string, len(register))
+	for reg, act := range register {
+		low[toRegDWORD(reg)] = toRegDWORD(act)
+	}
+	maps.Copy(register, low)
+}
+
+// selectRegister is used to make sure each register will be selected once.
+func (gen *Generator) selectRegister() string {
+	idx := gen.rand.Intn(len(gen.regBox))
+	reg := gen.regBox[idx]
+	// remove selected register
+	gen.regBox = append(gen.regBox[:idx], gen.regBox[idx+1:]...)
+	return reg
+}
+
+func (gen *Generator) insertGarbageInst() string {
+	if gen.opts.NoGarbage {
+		return ""
+	}
+	return ";" + toDB(gen.garbageInst())
 }
 
 func toDB(b []byte) string {

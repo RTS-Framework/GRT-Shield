ef09875ff22bc9acb8edcd7d6e898405790dc7a6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-09 13:46:50 +0800

    add TestGenerator for test generate x86/x64 shield stub, add invalid arch.

--- generator_test.go
+++ generator_test.go
@@ -1,1 +1,48 @@
 package shield
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerator(t *testing.T) {
+	generator := NewGenerator()
+
+	t.Run("x86", func(t *testing.T) {
+		ctx, err := generator.Generate(32, nil)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+		fmt.Println("seed:", ctx.Seed)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		ctx, err := generator.Generate(64, nil)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+		fmt.Println("seed:", ctx.Seed)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	t.Run("invalid arch", func(t *testing.T) {
+		ctx, err := generator.Generate(123, nil)
+		require.EqualError(t, err, "unsupported architecture: 123")
+		require.Nil(t, ctx)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}

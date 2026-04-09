9332eab8a659c177237345d93318c7a36ab36d12
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-09 13:10:20 +0800

    add testShield for call and test the generated shield stub.

---
+++ sc_others_test.go
@@ -0,0 +1,12 @@
+//go:build !windows
+
+package shield
+
+import (
+	"testing"
+	"time"
+)
+
+func testShield(t *testing.T, shield []byte, sleep time.Duration) {
+
+}
---
+++ sc_windows_test.go
@@ -0,0 +1,112 @@
+package shield
+
+import (
+	"debug/pe"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows"
+)
+
+var (
+	modKernel32 = syscall.NewLazyDLL("kernel32.dll")
+
+	procVirtualProtect       = modKernel32.NewProc("VirtualProtect")
+	procWaitForSingleObject  = modKernel32.NewProc("WaitForSingleObject")
+	procCreateWaitableTimerA = modKernel32.NewProc("CreateWaitableTimerA")
+	procSetWaitableTimer     = modKernel32.NewProc("SetWaitableTimer")
+)
+
+type testShieldArgs struct {
+	VirtualProtect      uintptr
+	WaitForSingleObject uintptr
+	CriticalAddress     uintptr
+	CriticalSize        uintptr
+	ShelterAddress      uintptr
+	Timer               uintptr
+	Key                 uintptr
+}
+
+func testShield(t *testing.T, shield []byte, sleep time.Duration) {
+	critical := make([]byte, 8192)
+	copy(critical, "runtime instruction")
+	criticalAddr := uintptr(unsafe.Pointer(&critical[0]))
+
+	address := testDeployShield(t, shield)
+	fmt.Printf("data address:   0x%X\n", criticalAddr)
+	fmt.Printf("shield address: 0x%X\n", address)
+	args := testBuildShieldArgs(t, critical, sleep)
+	now := time.Now()
+
+	_, _, _ = syscall.SyscallN(address, uintptr(unsafe.Pointer(args)))
+
+	require.Greater(t, time.Since(now), sleep)
+	require.True(t, strings.HasPrefix(string(critical), "runtime instruction"))
+}
+
+func testBuildShieldArgs(t *testing.T, critical []byte, sleep time.Duration) *testShieldArgs {
+	hTimer, _, err := procCreateWaitableTimerA.Call(0, 0, 0)
+	if hTimer == 0 {
+		require.NoError(t, err)
+	}
+	dueTime := -sleep.Milliseconds() * 1000 * 10
+	ok, _, err := procSetWaitableTimer.Call(
+		hTimer, uintptr(unsafe.Pointer(&dueTime)), 0, 0, 0, 1,
+	)
+	require.True(t, ok == 1, err)
+	ctx := &testShieldArgs{
+		CriticalAddress:     uintptr(unsafe.Pointer(&critical[0])),
+		CriticalSize:        uintptr(len(critical)),
+		VirtualProtect:      procVirtualProtect.Addr(),
+		WaitForSingleObject: procWaitForSingleObject.Addr(),
+		Timer:               hTimer,
+		Key:                 0x12345678,
+	}
+	return ctx
+}
+
+// try to write shield in .text section
+func testDeployShield(t *testing.T, shield []byte) uintptr {
+	exe, err := os.Executable()
+	require.NoError(t, err)
+	img, err := pe.Open(exe)
+	require.NoError(t, err)
+
+	text := img.Section(".text")
+	require.NotNil(t, text)
+	cave := text.VirtualSize - text.Size
+	if int(cave) < len(shield) {
+		return loadShellcode(t, shield)
+	}
+
+	peb := windows.RtlGetCurrentPeb()
+	address := peb.ImageBaseAddress + 0x1000 + uintptr(text.Size)
+	size := uintptr(len(shield))
+	var old uint32
+	err = windows.VirtualProtect(address, size, windows.PAGE_READWRITE, &old)
+	require.NoError(t, err)
+
+	dst := unsafe.Slice((*byte)(unsafe.Pointer(address)), size)
+	copy(dst, shield)
+
+	err = windows.VirtualProtect(address, size, old, &old)
+	require.NoError(t, err)
+	return address
+}
+
+func loadShellcode(t *testing.T, sc []byte) uintptr {
+	size := uintptr(len(sc))
+	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
+	mProtect := uint32(windows.PAGE_EXECUTE_READWRITE)
+	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
+	require.NoError(t, err)
+	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size)
+	copy(dst, sc)
+	return scAddr
+}

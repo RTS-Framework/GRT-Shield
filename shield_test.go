0ce6c9e85f8f5010e6444da3378ad3926303c9b4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-09 13:35:21 +0800

    add TestShield and testSleepTime for the common unit tests.

--- shield_test.go
+++ shield_test.go
@@ -1,1 +1,48 @@
 package shield
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testSleepTime = time.Second
+
+func TestShield(t *testing.T) {
+	generator := NewGenerator()
+
+	opts := &Options{
+		NoGarbage: true,
+		RandSeed:  1234,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		ctx, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		ctx, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}

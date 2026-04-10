cb9eb314fb7a6e3c2dd809bce26e2bd29e044d75
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-10 13:50:58 +0800

    add TestSpecificSeed and TestGeneratorFuzz for check generator.

--- generator_test.go
+++ generator_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -41,6 +42,84 @@ func TestGenerator(t *testing.T) {
 		ctx, err := generator.Generate(123, nil)
 		require.EqualError(t, err, "unsupported architecture: 123")
 		require.Nil(t, ctx)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}
+
+func TestSpecificSeed(t *testing.T) {
+	generator := NewGenerator()
+
+	opts := &Options{
+		RandSeed: 1234,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		ctx1, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		ctx2, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		require.Equal(t, ctx1, ctx2)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		testShield(t, ctx1.Output, testSleepTime)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		ctx1, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		ctx2, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		require.Equal(t, ctx1, ctx2)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		testShield(t, ctx1.Output, testSleepTime)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}
+
+func TestGeneratorFuzz(t *testing.T) {
+	const sleepTime = 30 * time.Millisecond
+
+	generator := NewGenerator()
+
+	t.Run("x86", func(t *testing.T) {
+		for i := 0; i < 100; i++ {
+			ctx, err := generator.Generate(32, nil)
+			require.NoError(t, err)
+			fmt.Println("size:", len(ctx.Output))
+			fmt.Println("seed:", ctx.Seed)
+
+			if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+				continue
+			}
+
+			testShield(t, ctx.Output, sleepTime)
+		}
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		for i := 0; i < 100; i++ {
+			ctx, err := generator.Generate(64, nil)
+			require.NoError(t, err)
+			fmt.Println("size:", len(ctx.Output))
+			fmt.Println("seed:", ctx.Seed)
+
+			if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+				continue
+			}
+
+			testShield(t, ctx.Output, sleepTime)
+		}
 	})
 
 	err := generator.Close()

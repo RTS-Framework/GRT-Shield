053d20801d7842818460d0356c2cfd68edd94be3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-10 13:19:39 +0800

    add TestGarbage and TestGarbageTemplateFuzz for check garbageInst().

--- garbage_test.go
+++ garbage_test.go
@@ -1,1 +1,78 @@
 package shield
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGarbage(t *testing.T) {
+	generator := NewGenerator()
+
+	opts := &Options{
+		NoGarbage: false,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		ctx, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		ctx, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		fmt.Println("size:", len(ctx.Output))
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		testShield(t, ctx.Output, testSleepTime)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}
+
+func TestGarbageTemplateFuzz(t *testing.T) {
+	t.Run("x86", func(t *testing.T) {
+		generator := NewGenerator()
+		generator.arch = 32
+		generator.opts = new(Options)
+		err := generator.initAssembler()
+		require.NoError(t, err)
+
+		for i := 0; i < 1000; i++ {
+			data := generator.garbageTemplate()
+			require.NotEmpty(t, data)
+		}
+
+		err = generator.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		generator := NewGenerator()
+		generator.arch = 64
+		generator.opts = new(Options)
+		err := generator.initAssembler()
+		require.NoError(t, err)
+
+		for i := 0; i < 1000; i++ {
+			data := generator.garbageTemplate()
+			require.NotEmpty(t, data)
+		}
+
+		err = generator.Close()
+		require.NoError(t, err)
+	})
+}

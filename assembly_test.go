e6023f0ca167890b17b51a9d418a38a5bf6ff8fb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-11 14:48:03 +0800

    move the tests about the assembly method to the new test file.

---
+++ assembly_test.go
@@ -0,0 +1,53 @@
+package shield
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestToDB(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		data := []byte{1, 2, 3, 4}
+		output := toDB(data)
+
+		expected := ".byte 0x01, 0x02, 0x03, 0x04, "
+		require.Equal(t, expected, output)
+	})
+
+	t.Run("empty bytes", func(t *testing.T) {
+		output := toDB(nil)
+		require.Empty(t, output)
+	})
+}
+
+func TestToHex(t *testing.T) {
+	output := toHex(15)
+	require.Equal(t, "0xF", output)
+}
+
+func TestToRegDWORD(t *testing.T) {
+	for _, item := range []*struct {
+		input  string
+		output string
+	}{
+		{"rax", "eax"},
+		{"rbx", "ebx"},
+		{"rcx", "ecx"},
+		{"rdx", "edx"},
+		{"rdi", "edi"},
+		{"rsi", "esi"},
+		{"rsp", "esp"},
+		{"r8", "r8d"},
+		{"r9", "r9d"},
+		{"r10", "r10d"},
+		{"r11", "r11d"},
+		{"r12", "r12d"},
+		{"r13", "r13d"},
+		{"r14", "r14d"},
+		{"r15", "r15d"},
+	} {
+		output := toRegDWORD(item.input)
+		require.Equal(t, item.output, output)
+	}
+}

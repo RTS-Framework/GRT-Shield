1f60661b3f0404730251fb2055b10991eca61317
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-11 13:10:47 +0800

    main.go: add loadJunkCodeTemplate, update main about Generate.

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/RTS-Framework/GRT-Shield"
 )
@@ -13,8 +15,9 @@ var (
 	jcx86 string
 	jcx64 string
 	opts  shield.Options
-	hex   bool
-	out   string
+
+	outHex  bool
+	outPath string
 )
 
 func init() {
@@ -25,8 +28,8 @@ func init() {
 	flag.StringVar(&opts.ShieldX64, "sid-x64", "", "specify the x64 shield template file path")
 	flag.StringVar(&jcx86, "junk-x86", "", "specify the x86 junk template directory path")
 	flag.StringVar(&jcx64, "junk-x64", "", "specify the x64 junk template directory path")
-	flag.BoolVar(&hex, "hex", false, "use hexadecimal encoding for output")
-	flag.StringVar(&out, "o", "", "specify the output file path")
+	flag.BoolVar(&outHex, "hex", false, "use hexadecimal encoding for output")
+	flag.StringVar(&outPath, "out", "", "specify the output file path")
 	flag.Parse()
 }
 
@@ -35,29 +38,63 @@ func main() {
 		flag.Usage()
 		return
 	}
-
-	if out == "" {
+	if outPath == "" {
 		setDefaultOutputName()
 	}
+	opts.JunkCodeX86 = loadJunkCodeTemplate(jcx86)
+	opts.JunkCodeX64 = loadJunkCodeTemplate(jcx64)
 
 	generator := shield.NewGenerator()
 
-	err := generator.Close()
+	ctx, err := generator.Generate(arch, &opts)
+	checkError(err)
+	fmt.Println("seed:", ctx.Seed)
+	fmt.Printf("save shield to \"%s\"\n", outPath)
+	output := ctx.Output
+	if outHex {
+		output = []byte(hex.EncodeToString(output))
+	}
+	err = os.WriteFile(outPath, output, 0600) // #nosec
+	checkError(err)
+
+	err = generator.Close()
 	checkError(err)
 }
 
 func setDefaultOutputName() {
 	switch arch {
 	case 32:
-		out = "shield_x86"
+		outPath = "shield_x86"
 	case 64:
-		out = "shield_x64"
+		outPath = "shield_x64"
 	}
-	if hex {
-		out += ".hex"
+	if outHex {
+		outPath += ".hex"
 	} else {
-		out += ".bin"
+		outPath += ".bin"
 	}
+}
+
+func loadJunkCodeTemplate(dir string) []string {
+	if dir == "" {
+		return nil
+	}
+	fmt.Println("load custom junk code template directory:", dir)
+	files, err := os.ReadDir(dir)
+	checkError(err)
+	templates := make([]string, 0, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		data, err := os.ReadFile(filepath.Join(dir, file.Name())) // #nosec
+		checkError(err)
+		src := string(data)
+		_, _, err = shield.InspectJunkCodeTemplate(arch, src)
+		checkError(err)
+		templates = append(templates, src)
+	}
+	return templates
 }
 
 func checkError(err error) {

d1efbe80d42db78ab6eb3e939f60ce7c0cebda4d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-13 15:11:47 +0800

    hybrid_x64.asm: improve template that support {{igi}} for polymorphism.

--- junk/hybrid_x64.asm
+++ junk/hybrid_x64.asm
@@ -1,29 +1,73 @@
 .code64
 
-pushfq
-push {{.Reg.rax}}
+entry:
+  pushfq
+  push {{.Reg.rax}}
 
-{{if .Switch.A}}
-mov {{.Reg.rax}}, {{.Reg.rbx}}
-{{end}}
+  {{if .Switch.A}}
+  mov {{.Reg.rax}}, {{.Reg.rbx}}
+  test {{.Reg.rax}}, {{.Reg.rax}}
+  jnz next_1
+  {{igi}}
+ next_1:
+  call func_1
+  {{end}}
 
-{{if .Switch.B}}
-xor {{.Reg.rax}}, {{.Reg.rcx}}
-{{end}}
+  {{if .Switch.B}}
+  xor {{.Reg.rax}}, {{.Reg.rcx}}
+  {{igi}}
+  jmp leave
+  {{end}}
 
-{{if .Switch.C}}
-xor {{.Reg.rax}}, {{.DWORD.D}}
-{{end}}
+  {{if .Switch.C}}
+  xor {{.Reg.rax}}, {{.DWORD.D}}
+  call func_2
+  {{end}}
 
-{{if .Switch.D}}
-mov {{.Reg.rax}}, {{.WORD.E}}
-{{end}}
+  {{if .Switch.D}}
+  mov {{.Reg.rax}}, {{.WORD.E}}
+  test {{.Reg.rax}}, {{.Reg.rax}}
+  jnz next_3
+  {{igi}}
+ next_3:
+  {{igi}}
+  jmp leave
+  {{end}}
 
-{{if .Switch.E}}
-ror {{.Reg.rax}}, {{.Less32.A}}
-xor {{.Reg.rax}}, {{.Reg.rdi}}
-rol {{.Reg.rax}}, {{.Less32.B}}
-{{end}}
+  {{if .Switch.E}}
+  ror {{.Reg.rax}}, {{.Less32.A}}
+  xor {{.Reg.rax}}, {{.Reg.rdi}}
+  rol {{.Reg.rax}}, {{.Less32.B}}
+  call func_3
+  {{end}}
 
-pop {{.Reg.rax}}
-popfq
+leave:
+  pop {{.Reg.rax}}
+  popfq
+  jmp exit
+
+func_1:
+  {{igi}}
+  ret
+
+func_2:
+  push {{.Reg.rbx}}
+  mov {{.Reg.rbx}}, {{.Reg.rcx}}
+  {{if .Switch.F}}
+    test {{.Reg.rbx}}, {{.Reg.rbx}}
+    jnz next_2
+    {{igi}}
+   next_2:
+    call func_1
+  {{end}}
+  pop {{.Reg.rbx}}
+  ret
+
+func_3:
+  rol {{.Reg.rax}}, {{.Less32.C}}
+  {{if .Switch.E}}
+  {{igi}}
+  {{end}}
+  ret
+
+exit:

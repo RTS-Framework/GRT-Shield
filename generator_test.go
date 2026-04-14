fda8d94879a94db1544a0fb062dcc6355eec6812
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-14 12:42:09 +0800

    update TestGeneratorFuzz for check the output shield is too large.

--- generator_test.go
+++ generator_test.go
@@ -98,6 +98,7 @@ func TestGeneratorFuzz(t *testing.T) {
 			require.NoError(t, err)
 			fmt.Println("size:", len(ctx.Output))
 			fmt.Println("seed:", ctx.Seed)
+			require.Less(t, len(ctx.Output), 4096, "shield is too large")
 
 			if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
 				continue
@@ -113,6 +114,7 @@ func TestGeneratorFuzz(t *testing.T) {
 			require.NoError(t, err)
 			fmt.Println("size:", len(ctx.Output))
 			fmt.Println("seed:", ctx.Seed)
+			require.Less(t, len(ctx.Output), 4096, "shield is too large")
 
 			if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
 				continue

9f18b13c91d9aaa5a53c10dce898caa9e5064b38
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-20 12:20:04 +0800

    improve testShield, check the shield is empty, prevent do nothing.

--- sc_others_test.go
+++ sc_others_test.go
@@ -5,8 +5,10 @@ package shield
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func testShield(t *testing.T, shield []byte, sleep time.Duration) {
-
+	require.NotEmpty(t, shield)
 }

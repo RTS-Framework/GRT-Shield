e3ec9fdea579fb8b0d909cc710ff152e5e9cd31d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-24 14:08:40 +0800

    move methods about map register, update methods about build register map.

--- assembly.go
+++ assembly.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
+	"maps"
 	"slices"
 	"strconv"
 	"strings"
@@ -52,9 +53,15 @@ func (gen *Generator) buildRandomRegisterMap() map[string]string {
 		registers = registerX64
 	}
 	reg := gen.shuffleRegisterMap(registers)
+	word := gen.buildWORDRegisterMap(reg)
+	b := gen.buildBYTERegisterMap(reg)
+	high := gen.buildHigh8BitRegisterMap(reg)
 	if gen.arch == 64 {
-		gen.buildDWORDBitRegisterMap(reg)
+		maps.Copy(reg, gen.buildDWORDBitRegisterMap(reg))
 	}
+	maps.Copy(reg, word)
+	maps.Copy(reg, b)
+	maps.Copy(reg, high)
 	return reg
 }
 
@@ -67,9 +74,15 @@ func (gen *Generator) buildVolatileRegisterMap() map[string]string {
 		registers = regVolatileX64
 	}
 	reg := gen.shuffleRegisterMap(registers)
+	word := gen.buildWORDRegisterMap(reg)
+	b := gen.buildBYTERegisterMap(reg)
+	high := gen.buildHigh8BitRegisterMap(reg)
 	if gen.arch == 64 {
-		gen.buildDWORDBitRegisterMap(reg)
+		maps.Copy(reg, gen.buildDWORDBitRegisterMap(reg))
 	}
+	maps.Copy(reg, word)
+	maps.Copy(reg, b)
+	maps.Copy(reg, high)
 	return reg
 }
 
@@ -82,9 +95,15 @@ func (gen *Generator) buildNonvolatileRegisterMap() map[string]string {
 		registers = regNonvolatileX64
 	}
 	reg := gen.shuffleRegisterMap(registers)
+	word := gen.buildWORDRegisterMap(reg)
+	b := gen.buildBYTERegisterMap(reg)
+	high := gen.buildHigh8BitRegisterMap(reg)
 	if gen.arch == 64 {
-		gen.buildDWORDBitRegisterMap(reg)
+		maps.Copy(reg, gen.buildDWORDBitRegisterMap(reg))
 	}
+	maps.Copy(reg, word)
+	maps.Copy(reg, b)
+	maps.Copy(reg, high)
 	return reg
 }
 
@@ -152,82 +171,6 @@ func (gen *Generator) buildHigh8BitRegisterMap(regMap map[string]string) map[str
 		high[s] = d
 	}
 	return high
-}
-
-func printInstructions(src []byte, mode int) (string, string, error) {
-	binHex := strings.Builder{}
-	insts := strings.Builder{}
-	for len(src) > 0 {
-		inst, err := x86asm.Decode(src, mode)
-		if err != nil {
-			return "", "", err
-		}
-		b := src[:inst.Len]
-		binHex.WriteString(printAssemblyBinHex(&inst, b))
-		binHex.Write([]byte("\r\n"))
-		insts.WriteString(printAssemblyInstruction(&inst))
-		insts.Write([]byte("\r\n"))
-		src = src[inst.Len:]
-	}
-	return binHex.String(), insts.String(), nil
-}
-
-func printAssemblyBinHex(inst *x86asm.Inst, b []byte) string {
-	var bin strings.Builder
-	switch {
-	case inst.PCRelOff != 0:
-		s1 := strings.ToUpper(hex.EncodeToString(b[:inst.PCRelOff]))
-		s2 := strings.ToUpper(hex.EncodeToString(b[inst.PCRelOff:]))
-		bin.WriteString(s1)
-		bin.WriteString(" ")
-		bin.WriteString(s2)
-	default:
-		s := strings.ToUpper(hex.EncodeToString(b))
-		bin.WriteString(s)
-	}
-	return bin.String()
-}
-
-func printAssemblyInstruction(inst *x86asm.Inst) string {
-	var buf bytes.Buffer
-	for _, p := range inst.Prefix {
-		if p == 0 {
-			break
-		}
-		if p&x86asm.PrefixImplicit != 0 {
-			continue
-		}
-		_, _ = fmt.Fprintf(&buf, "%s ", strings.ToLower(p.String()))
-	}
-	_, _ = fmt.Fprintf(&buf, "%s", strings.ToLower(inst.Op.String()))
-	sep := " "
-	for _, v := range inst.Args {
-		if v == nil {
-			break
-		}
-		_, _ = fmt.Fprintf(&buf, "%s%s", sep, strings.ToLower(v.String()))
-		sep = ", "
-	}
-	return buf.String()
-}
-
-func toDB(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-	builder := strings.Builder{}
-	builder.WriteString(".byte ")
-	for i := 0; i < len(b); i++ {
-		builder.WriteString("0x")
-		s := hex.EncodeToString([]byte{b[i]})
-		builder.WriteString(strings.ToUpper(s))
-		builder.WriteString(", ")
-	}
-	return builder.String()
-}
-
-func toHex(v any) string {
-	return fmt.Sprintf("0x%X", v)
 }
 
 // map r8 -> r8d, rax -> eax
@@ -309,4 +252,80 @@ func toRegHigh8Bit(reg string) string {
 	default:
 		return ""
 	}
+}
+
+func printInstructions(src []byte, mode int) (string, string, error) {
+	binHex := strings.Builder{}
+	insts := strings.Builder{}
+	for len(src) > 0 {
+		inst, err := x86asm.Decode(src, mode)
+		if err != nil {
+			return "", "", err
+		}
+		b := src[:inst.Len]
+		binHex.WriteString(printAssemblyBinHex(&inst, b))
+		binHex.Write([]byte("\r\n"))
+		insts.WriteString(printAssemblyInstruction(&inst))
+		insts.Write([]byte("\r\n"))
+		src = src[inst.Len:]
+	}
+	return binHex.String(), insts.String(), nil
+}
+
+func printAssemblyBinHex(inst *x86asm.Inst, b []byte) string {
+	var bin strings.Builder
+	switch {
+	case inst.PCRelOff != 0:
+		s1 := strings.ToUpper(hex.EncodeToString(b[:inst.PCRelOff]))
+		s2 := strings.ToUpper(hex.EncodeToString(b[inst.PCRelOff:]))
+		bin.WriteString(s1)
+		bin.WriteString(" ")
+		bin.WriteString(s2)
+	default:
+		s := strings.ToUpper(hex.EncodeToString(b))
+		bin.WriteString(s)
+	}
+	return bin.String()
+}
+
+func printAssemblyInstruction(inst *x86asm.Inst) string {
+	var buf bytes.Buffer
+	for _, p := range inst.Prefix {
+		if p == 0 {
+			break
+		}
+		if p&x86asm.PrefixImplicit != 0 {
+			continue
+		}
+		_, _ = fmt.Fprintf(&buf, "%s ", strings.ToLower(p.String()))
+	}
+	_, _ = fmt.Fprintf(&buf, "%s", strings.ToLower(inst.Op.String()))
+	sep := " "
+	for _, v := range inst.Args {
+		if v == nil {
+			break
+		}
+		_, _ = fmt.Fprintf(&buf, "%s%s", sep, strings.ToLower(v.String()))
+		sep = ", "
+	}
+	return buf.String()
+}
+
+func toDB(b []byte) string {
+	if len(b) == 0 {
+		return ""
+	}
+	builder := strings.Builder{}
+	builder.WriteString(".byte ")
+	for i := 0; i < len(b); i++ {
+		builder.WriteString("0x")
+		s := hex.EncodeToString([]byte{b[i]})
+		builder.WriteString(strings.ToUpper(s))
+		builder.WriteString(", ")
+	}
+	return builder.String()
+}
+
+func toHex(v any) string {
+	return fmt.Sprintf("0x%X", v)
 }

bebca524016bd49ebf651b192771b665d3263327
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-25 14:28:53 +0800

    improve Generate methods about process empty template and output.

--- generator.go
+++ generator.go
@@ -136,14 +136,20 @@ func (gen *Generator) Generate(arch int, opts *Options) (ctx *Context, err error
 		seed = seed & 0x7FFFFFFFFFFFFFFF
 	}
 	gen.rand.Seed(seed)
-	// build shield source from template
+	// build shield from template
 	shield, err := gen.buildShield("")
 	if err != nil {
 		return nil, fmt.Errorf("failed to build shield: %s", err)
 	}
+	if len(shield) == 0 {
+		return nil, fmt.Errorf("empty output shield assembly source")
+	}
 	output, err := gen.assemble(shield)
 	if err != nil {
 		return nil, fmt.Errorf("failed to assemble shield source: %s", err)
+	}
+	if len(output) == 0 {
+		return nil, fmt.Errorf("assemble shield source with unknown error")
 	}
 	if len(output) > MaxShieldSize {
 		return nil, ErrShieldSizeTooLarge

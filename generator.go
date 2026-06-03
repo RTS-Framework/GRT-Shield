dd58f095ef6c299f0311994e4802e69869768445
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-03 12:50:22 +0800

    remove the structure field prefix about shield in Context.

--- generator.go
+++ generator.go
@@ -61,10 +61,10 @@ type Options struct {
 
 // Context contains the output and context data in Generate.
 type Context struct {
-	Output     []byte `json:"output"`
-	ShieldHex  string `json:"shield_hex"`
-	ShieldInst string `json:"shield_inst"`
-	Seed       int64  `json:"seed"`
+	Output []byte `json:"output"`
+	Hex    string `json:"hex"`
+	Inst   string `json:"inst"`
+	Seed   int64  `json:"seed"`
 }
 
 // NewGenerator is used to create a shield generator.
@@ -157,10 +157,10 @@ func (gen *Generator) Generate(arch int, opts *Options) (ctx *Context, err error
 	}
 	// build context for test and debug
 	ctx = &Context{
-		Output:     output,
-		ShieldHex:  binHex,
-		ShieldInst: insts,
-		Seed:       seed,
+		Output: output,
+		Hex:    binHex,
+		Inst:   insts,
+		Seed:   seed,
 	}
 	return ctx, nil
 }
